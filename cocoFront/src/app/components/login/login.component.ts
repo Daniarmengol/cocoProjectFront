c243ab543a62ae1090b7628a3310f3583bd43c0a
Dani Fernández Armengol <danielito486@gmail.com>
2022-07-18 16:49:48 +0200

    creado servicio e interfaz de usuarios, pendiente de funcionar validaciones del login

--- cocoFront/src/app/components/login/login.component.ts
+++ cocoFront/src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/interfaces/user.interface';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private usersService: UsersService,
+    private router: Router
+  ) {
+    this.loginForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(15)
+      ]),
+
+      password: new FormControl('', [
+        Validators.required
+      ]),
+    })
+  }
 
   ngOnInit(): void {
   }
+
+  async getDataForm(formData: any) {
+    /* console.log(this.loginForm.value) */
+    try {
+      const response: User | any = await this.usersService.login(this.loginForm.value)
+      console.log(response);
+      if (response.token) {
+        localStorage.setItem('user-token', response.token)
+        this.router.navigate(['/home', response.token])
+      }
+
+    } catch (error) {
+
+    }
+
+
+
+  }
+
+  /* 
+    async getDataForm(pForm: any) {
+      //tenemos enviar los datos a la api a traves del servicio.
+      try {
+        const response: User | any = await this.usersService.login(pForm.value)
+        console.log(response);
+        if (response.token) {
+          localStorage.setItem('user-token', response.token)
+          this.router.navigate(['/premio', response.token])
+        }
+        else {
+          alert(response.error)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    } */
+
+
 
 }
---
+++ cocoFront/src/app/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: number;
+    username: string;
+    email: string;
+    password: string;
+    direccion: string;
+    nombre: string;
+    apellidos: string;
+    fecha_nacimiento: Date;
+}
