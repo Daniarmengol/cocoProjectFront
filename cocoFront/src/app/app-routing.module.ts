89fbb238f91aed4f3e5ba828da9e0ef4e725c6fc
Dani Fernández Armengol <danielito486@gmail.com>
2022-07-18 18:08:44 +0200

    cositas variopintas, guard del token en login

--- cocoFront/src/app/app-routing.module.ts
+++ cocoFront/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { LoginGuard } from './guards/login.guard';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: '', pathMatch: 'full', redirectTo: '/home' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [LoginGuard] }
 
-  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
--- cocoFront/src/app/guards/login.guard.ts
+++ cocoFront/src/app/guards/login.guard.ts
@@ -19,16 +19,5 @@ export class LoginGuard implements CanActivate {
     return true;
   }
 
-
-  /* canActivate(): boolean {
-    console.log('paso por el guard')
-    let token: string | null = localStorage.getItem('user-token')
-    if (token === null) {
-      //redirigir a la pagina de login.
-      this.router.navigate(['/login'])
-      return false
-    }
-    return true; */
-
 }
 
