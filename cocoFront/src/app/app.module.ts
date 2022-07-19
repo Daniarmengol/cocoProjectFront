dd14064d67970aa5cc07bda59c7943e177c84814
J1Loop <jorge.ua.1996@gmail.com>
2022-07-19 14:09:10 +0200

    Estructura footer terminada. Routing sin definir, falta discutirlo. Apunto en trello.

--- cocoFront/src/app/app.module.ts
+++ cocoFront/src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
