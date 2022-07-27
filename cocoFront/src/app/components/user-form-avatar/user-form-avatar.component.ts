6cee95066c54482eb487fe528b1f0af136fa97ab
J1Loop <jorge.ua.1996@gmail.com>
2022-07-27 13:21:34 +0200

    Creado user form avatar para los edits necesarios desde el perfil de usuario. Commit para coger el modal de Pedro Instalado Sweet alerts

---
+++ cocoFront/src/app/components/user-form-avatar/user-form-avatar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form-avatar',
+  templateUrl: './user-form-avatar.component.html',
+  styleUrls: ['./user-form-avatar.component.css']
+})
+export class UserFormAvatarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
